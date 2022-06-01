7127e202725db11172275075dd378d8f5ea84b94
Netson <netsoncavina@gmail.com>
2022-05-31 21:47:21 -0300

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import "./header.css";
+export default function Header() {
+  return (
+    <header>
+      <div class="container">
+        <h2 class="logo">TRAILERFLIX</h2>
+        <nav>
+          <a href="#">Inicio</a>
+          <a href="#">Series</a>
+          <a href="#">Filmes</a>
+          <a href="#">Documentarios</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
