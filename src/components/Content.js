b9ce74b8c381cd69143817de29abff86689c487d
Netson <netsoncavina@gmail.com>
2022-06-06 01:29:23 -0300

    update mainMovie style

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,6 +5,9 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import movieData from "../movies";
 export default function Content() {
+  function getWallpaper(title) {
+    return `https://www.omdbapi.com/?t=${title}&apikey=6022c906`;
+  }
   const movies = movieData.map((movie) => {
     return (
       <MovieCard key={movie.id} title={movie.title} poster={movie.poster} />
--- src/components/MainMovie.js
+++ src/components/MainMovie.js
@@ -1,22 +1,46 @@
 import "./styles/mainMovie.css";
 import "./styles/responsive.css";
-export default function MainMovie() {
+import { useState } from "react";
+export default function MainMovie(props) {
+  const [title, setTitle] = useState("House of Cards");
+  const [wallpaper, setWallpaper] = useState(
+    "https://wallpapercave.com/wp/wp1839801.jpg"
+  );
+  const [description, setDescription] = useState(
+    "Um político inescrupuloso não mede esforços para conquistar o poder nos EUA neste drama vencedor do Emmy e do Globo de Ouro."
+  );
+  const [trailer, setTrailer] = useState(
+    "https://www.youtube.com/watch?v=8QnMmpfKWvo"
+  );
+  if (props.title) {
+    setTitle(props.title);
+  }
+  if (props.wallpaper) {
+    setWallpaper(props.wallpaper);
+  }
+  if (props.description) {
+    setDescription(props.description);
+  }
+  if (props.trailer) {
+    setTrailer(props.trailer);
+  }
+
   return (
-    <div className="filme-principal" id="filme-principal">
+    <div
+      className="filme-principal"
+      id="filme-principal"
+      style={{ backgroundImage: `url(${wallpaper})` }}
+    >
       <div className="container">
         <h3 className="titulo" id="titulo">
-          House of Cards
+          {title}
         </h3>
         <p className="descricao" id="sinopse">
-          Um político inescrupuloso não mede esforços para conquistar o poder
-          nos EUA neste drama vencedor do Emmy e do Globo de Ouro.
+          {description}
         </p>
+
         <div className="botoes">
-          <a
-            className="popup"
-            id="trailer"
-            href="https://www.youtube.com/watch?v=8QnMmpfKWvo"
-          >
+          <a className="popup" id="trailer" href={trailer}>
             <button role="button" className="botao">
               <i className="fas fa-play"></i>
               ASSISTIR TRAILER
